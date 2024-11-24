d872d0ded7eee6e0b989a5d66ae7eaddf9478995
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-11-24 16:38:25 +0700

    feat: Add type.d.ts

---
+++ asset/resource/type.d.ts
@@ -0,0 +1,29 @@
+declare module '*.png' {
+  const value: number;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  const value: number;
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: number;
+  export = value;
+}
+
+declare module '*.svg' {
+  const value: number;
+  export = value;
+}
+
+declare module '*.webp' {
+  const value: number;
+  export = value;
+}
+
+declare module '*.gif' {
+  const value: number;
+  export = value;
+}

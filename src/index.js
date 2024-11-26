4525ff9bfd7c52fa14731bfcfd00f299b568b36b
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-11-26 21:48:40 +0700

    refactor: Remove 'generateContent()' from index.ts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-import dinosaur from "../asset/images/dinosaur.png";
 import { generateHomeContent } from "./home";
-const generateContent = () => {
-    const content = document.querySelector("div#content");
-    const img = document.createElement("img");
-    const p = document.createElement("p");
-    const h1 = document.createElement("h1");
-    img.src = `${dinosaur}`;
-    p.textContent = "Hello, world! Nice to you all here.";
-    h1.textContent = "Hello, world!";
-    content.appendChild(img);
-    content.appendChild(p);
-    content.appendChild(h1);
-};
 const handleButtons = () => {
     const home = document.querySelector("header > nav > button:nth-child(1)");
     const menu = document.querySelector("header > nav > button:nth-child(2)");
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
 import dinosaur from "../asset/images/dinosaur.png";
 import { generateHomeContent } from "./home";
 
-type GenerateContent = () => void;
-const generateContent:GenerateContent = () => {
-  const content = document.querySelector("div#content") as HTMLElement;
-  const img = document.createElement("img");
-  const p = document.createElement("p");
-  const h1 = document.createElement("h1");
-
-  img.src = `${dinosaur}`;
-  p.textContent = "Hello, world! Nice to you all here.";
-  h1.textContent = "Hello, world!";
-
-  content.appendChild(img);
-  content.appendChild(p);
-  content.appendChild(h1);
-};
-
 const handleButtons = () => {
   const home = document.querySelector("header > nav > button:nth-child(1)") as HTMLElement;
   const menu = document.querySelector("header > nav > button:nth-child(2)") as HTMLElement;

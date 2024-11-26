4927946cdd373fa96e631c75c1e58547bd74bf55
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-11-26 13:08:04 +0700

    feat: Implement createMainBanner(description: string), which returns a <div> element with a text content of 'description'

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
-"use strict";
+export const createMainBanner = (description) => {
+    const MAIN_BANNER = document.createElement("div");
+    MAIN_BANNER.setAttribute("class", ".main-banner");
+    MAIN_BANNER.textContent = `${description}`;
+    return MAIN_BANNER;
+};

5822feb83ae183c125207bf1b534dd4fd029b05c
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-11-27 08:28:01 +0700

    feat: Update 'handleButtons()', so it also apply 'click' event that fires 'generateMenuContent()'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { generateHomeContent } from "./home";
+import { generateMenuContent } from "./menu";
 import "../styles/styles.css";
 const handleButtons = () => {
     const home = document.querySelector("header > nav > button:nth-child(1)");
     const menu = document.querySelector("header > nav > button:nth-child(2)");
     const about = document.querySelector("header > nav > button:nth-child(3)");
     home.addEventListener("click", generateHomeContent);
+    menu.addEventListener("click", generateMenuContent);
 };
 handleButtons();

cdd5ead59564c3b8473a6d347b883ca6f8471b9f
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-11-28 12:49:14 +0700

    feat: organize and export image paths for menu items

---
+++ asset/data/images.ts
@@ -0,0 +1,14 @@
+import HONEY_TEA from "../images/honey-tea.jpg";
+import BEARY_TEA from "../images/beary-tea.jpeg";
+import PANCAKES from "../images/pancakes.jpg";
+import FRENCH_TOAST from "../images/french-toast.jpg";
+import BEARY_VEGGIE_SANDWICH from "../images/veggie-sandwich.jpeg";
+import BLT from "../images/blt.jpeg";
+import BAGEL_AND_LOX from "../images/bagel-and-lox.webp";
+import HONEYCOMB from "../images/honeycomb.jpeg";
+import BEARY_BOWL from "../images/beary-bowl.jpg";
+import THE_BEARY_BEST_PORRIDGE from "../images/the-beary-bowl-best-porridge.jpeg";
+
+export const imagePaths = {
+  HONEY_TEA, BEARY_TEA, PANCAKES, FRENCH_TOAST, BEARY_VEGGIE_SANDWICH, BLT, BAGEL_AND_LOX, HONEYCOMB, BEARY_BOWL, THE_BEARY_BEST_PORRIDGE,
+};

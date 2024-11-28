830e161f5b40122d8750e9c7faad3b128e527d42
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-11-28 13:17:36 +0700

    refactor: remove . from main-banner

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const createMainBanner = (description) => {
     const MAIN_BANNER = document.createElement("div");
-    MAIN_BANNER.setAttribute("class", ".main-banner");
+    MAIN_BANNER.setAttribute("class", "main-banner");
     MAIN_BANNER.textContent = `${description}`;
     return MAIN_BANNER;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ type CreateMainBanner = (description: string) => HTMLElement;
 export const createMainBanner: CreateMainBanner = (description) =>  {
   const MAIN_BANNER = document.createElement("div");
 
-  MAIN_BANNER.setAttribute("class", ".main-banner");
+  MAIN_BANNER.setAttribute("class", "main-banner");
   MAIN_BANNER.textContent = `${description}`;
 
   return MAIN_BANNER;

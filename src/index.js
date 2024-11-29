4ddbae92eca1cafd3eff1d39abc42c59f5449f2f
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-11-29 11:59:52 +0700

    feat: Add ABOUT_BUTTON click event to generate about content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { generateHomeContent } from "./home";
 import { generateMenuContent } from "./menu";
 import "../styles/styles.css";
+import { generateAboutContent } from "./about";
 const handleButtons = () => {
     const HOME_BUTTON = document.querySelector("header > nav > button:nth-child(1)");
     const MENU_BUTTON = document.querySelector("header > nav > button:nth-child(2)");
     const ABOUT_BUTTON = document.querySelector("header > nav > button:nth-child(3)");
     HOME_BUTTON.addEventListener("click", generateHomeContent);
     MENU_BUTTON.addEventListener("click", generateMenuContent);
+    ABOUT_BUTTON.addEventListener("click", generateAboutContent);
 };
 handleButtons();
